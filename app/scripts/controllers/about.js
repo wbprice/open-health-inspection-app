60d32e29f1d934003e7b388909dba030efbd5767
Blaine Price <1wbprice@gmail.com>
2015-02-26 11:01:58 -0500

    Add all the things.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openHealthInspectionAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the openHealthInspectionAppApp
+ */
+angular.module('openHealthInspectionAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openHealthInspectionAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the openHealthInspectionAppApp
+ */
+angular.module('openHealthInspectionAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
