a58ee9214648882eb32b022a7563288d4dd5f594
Blaine Price <1wbprice@gmail.com>
2015-02-26 11:34:00 -0500

    Add geolocation service.

---
+++ app/scripts/services/geolocation.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name openHealthInspectionAppApp.geolocation
+ * @description
+ * # geolocation
+ * Service in the openHealthInspectionAppApp.
+ */
+angular.module('openHealthInspectionAppApp')
+  .service('geolocation', ['$q','$timeout', function ($q, $timeout) {
+    return {
+
+      getPosition: function() {
+
+        var deferred, countdown;
+
+        deferred = q.defer();
+        countdown = function() {
+          deferred.reject('The request to get the user\'s location timed out.');
+        };
+        $timeout(countdown, 5000);
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            $timeout.cancel(countdown);
+              deferred.resolve(position);
+            },
+            function(error) {
+
+              var errorCode;
+
+              $timeout.cancel(countdown);
+              switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    errorCode = 'User denied the request for Geolocation.';
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    errorCode = 'Location information is unavailable.';
+                    break;
+                case error.TIMEOUT:
+                    errorCode = 'The request to get user location timed out.';
+                    break;
+                case error.UNKNOWN_ERROR:
+                    errorCode = 'An unknown error occurred.';
+                    break;
+              }
+
+              deferred.reject(errorCode);
+
+            });
+
+            return deferred.promise;
+
+      }
+    }
+  };
+}]);
